84eb3f3060a0deea56595855ba7ad5f7ce213712
amos <amosmokua12@gmail.com>
2024-06-27 17:12:42 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Silver Stick';
+
+const welcomeMessage ='Welcome to Flatbook, Silver Stick!';
+
+ const excitedWelcomeMessage ='WELCOME TO FLATBOOK, SILVER STICK!';
+
+const shortGreeting = "Welcome, Silver!";
